99d73c03676308e0c4b4fd358fb0beca9dfd0d60
Naum <nvlavceski542@gmail.com>
2025-08-05 16:58:26 +0200

    Mobile-Version

--- main.js
+++ main.js
@@ -1,21 +1,34 @@
+
+
 function change_theme(){
         document.body.classList.toggle("dark")
-        if(document.getElementsByClassName("logo").item(0).attributes.item(0).value=="Light_Naum.png"){
+        if(document.getElementsByClassName("logo").item(0).attributes.item(0).value==="Light_Naum.png"){
                 document.getElementsByClassName("logo").item(0).attributes.item(0).value="Dark_Naum.png"
+                console.log(document.getElementsByClassName("logo").item(1).attributes.item(0).value="Dark_Naum.png")
+                document.getElementsByClassName("logo").item(2).attributes.item(0).value="Dark_Naum.png"
         }else{
                  document.getElementsByClassName("logo").item(0).attributes.item(0).value="Light_Naum.png"
+                document.getElementsByClassName("logo").item(1).attributes.item(0).value="Light_Naum.png"
+                document.getElementsByClassName("logo").item(2).attributes.item(0).value="Light_Naum.png"
+
         }
 
 }
 document.getElementById("about_me").hidden=true
 
-function about_me(){
-        document.getElementById("about_me").hidden=false
-        document.getElementById("home").hidden=true
-
-
+function  about_me(){
+        document.getElementById("about_me").classList.add("active")
+        document.getElementById("home").classList.remove("active")
+        setTimeout(function () {
+                document.getElementById("about_me").hidden = false
+                document.getElementById("home").hidden = true
+        }, 500)
 }
 function home(){
+        document.getElementById("about_me").classList.remove("active")
+        document.getElementById("home").classList.add("active")
+         setTimeout(function (){
         document.getElementById("about_me").hidden=true
         document.getElementById("home").hidden=false
+        },500)
 }
