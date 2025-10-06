87104590b7a3105a7f95d4399fcca2c3a1a5586c
Naum <nvlavceski542@gmail.com>
2025-10-06 13:58:49 +0200

    v2.0

--- js/main.js
+++ js/main.js
@@ -1,23 +1,39 @@
-
 function toggleLanguage() {
     document.querySelectorAll(".lang").forEach(el => el.classList.toggle("active"));
-    if (document.getElementById("changeLangue").innerHTML==="<span>EN</span> <i class=\"fa-solid fa-globe\"></i>"){
-            document.getElementById("changeLangue").innerHTML="<span>MK</span> <i class=\"fa-solid fa-globe\"></i>"
-    }else{
-            document.getElementById("changeLangue").innerHTML="<span>EN</span> <i class=\"fa-solid fa-globe\"></i>"
+    if (document.getElementById("changeLangue").innerHTML === "<span id=\"najdi\">EN</span> <i class=\"fa-solid fa-globe\"></i>") {
+        document.getElementById("changeLangue").innerHTML = "<span id=\"najdi\">MK</span> <i class=\"fa-solid fa-globe\"></i>"
+    } else {
+        document.getElementById("changeLangue").innerHTML = "<span id=\"najdi\">EN</span> <i class=\"fa-solid fa-globe\"></i>"
     }
 }
-function showSection(sectionId,button) {
-     document.querySelectorAll(".section").forEach(el => el.classList.remove("active"));
+
+function showSection(sectionId, button) {
+    document.querySelectorAll(".section").forEach(el => el.classList.remove("active"));
     let mkSection = document.getElementById(sectionId);
-    if (mkSection) mkSection.classList.add("active");
+    if (mkSection) {
+        mkSection.classList.add("active");
+        // mkSection.classList.add("opacity")
+    }
     let enSection = document.getElementById(sectionId + "_en");
-    if (enSection) enSection.classList.add("active");
-    document.querySelectorAll(".nav-link").forEach(el=>el.classList.remove("active"))
-        button.classList.add("active")
+    if (enSection) {
+        enSection.classList.add("active");
+        // enSection.classList.add("opacity")
+    }
+
+    document.querySelectorAll(".nav-link").forEach(el => el.classList.remove("active"))
+    button.classList.add("active")
+    if (document.getElementsByClassName("nav-link")[0] === button) {
+        enSection.classList.add("slide-Left")
+    } else {
+        enSection.classList.add("slide-Right")
+    }
 }
 
+function toggleActive(button) {
+    document.querySelectorAll(".nav-link").forEach(el => el.classList.remove("active"))
+    button.classList.add("active")
+}
 
 function change_theme() {
-        document.body.classList.toggle("dark");
+    document.body.classList.toggle("dark")
 }
